834a8da0891c37e87f62197cfb38d8888dfee960
vmihaylov <vasiliy.mikhailov@gmail.com>
2025-10-04 18:51:48 +0300

    initial commit

--- src/update_quality.sql
+++ src/update_quality.sql
@@ -21,15 +21,11 @@ RETURNS TABLE(new_quality numeric, new_sell_in numeric)
 LANGUAGE plpgsql
 AS $$
 DECLARE
-  l_new_quality numeric := p_quality;
   l_new_sell_in numeric := p_sell_in - 1;
   l_delta integer := CASE WHEN p_is_conjured THEN 2 ELSE 1 END;
+  l_total_delta integer := l_delta + (CASE WHEN l_new_sell_in < 0 THEN l_delta ELSE 0 END);
 BEGIN
-  l_new_quality := decrease_quality(l_new_quality, l_delta);
-  IF l_new_sell_in < 0 THEN
-    l_new_quality := decrease_quality(l_new_quality, l_delta);
-  END IF;
-  RETURN QUERY SELECT l_new_quality, l_new_sell_in;
+  RETURN QUERY SELECT decrease_quality(p_quality, l_total_delta), l_new_sell_in;
 END;
 $$;
 
@@ -38,15 +34,11 @@ RETURNS TABLE(new_quality numeric, new_sell_in numeric)
 LANGUAGE plpgsql
 AS $$
 DECLARE
-  l_new_quality numeric := p_quality;
   l_new_sell_in numeric := p_sell_in - 1;
   l_delta integer := CASE WHEN p_is_conjured THEN 2 ELSE 1 END;
+  l_total_delta integer := l_delta + (CASE WHEN l_new_sell_in < 0 THEN l_delta ELSE 0 END);
 BEGIN
-  l_new_quality := increase_quality(l_new_quality, l_delta);
-  IF l_new_sell_in < 0 THEN
-    l_new_quality := increase_quality(l_new_quality, l_delta);
-  END IF;
-  RETURN QUERY SELECT l_new_quality, l_new_sell_in;
+  RETURN QUERY SELECT increase_quality(p_quality, l_total_delta), l_new_sell_in;
 END;
 $$;
 
@@ -58,14 +50,12 @@ DECLARE
   l_new_quality numeric := p_quality;
   l_new_sell_in numeric := p_sell_in - 1;
   l_delta integer := CASE WHEN p_is_conjured THEN 2 ELSE 1 END;
+  l_extra integer := 0;
 BEGIN
   l_new_quality := increase_quality(l_new_quality, l_delta);
-  IF p_sell_in < 11 THEN
-    l_new_quality := increase_quality(l_new_quality, l_delta);
-  END IF;
-  IF p_sell_in < 6 THEN
-    l_new_quality := increase_quality(l_new_quality, l_delta);
-  END IF;
+  IF p_sell_in < 11 THEN l_extra := l_extra + l_delta; END IF;
+  IF p_sell_in < 6 THEN l_extra := l_extra + l_delta; END IF;
+  l_new_quality := increase_quality(l_new_quality, l_extra);
   IF l_new_sell_in < 0 THEN
     l_new_quality := 0;
   END IF;
@@ -96,33 +86,17 @@ DECLARE
   l_new_quality numeric;
   l_new_sell_in numeric;
 BEGIN
-  FOR l_item IN
-    SELECT name, sell_in, quality, ctid FROM item FOR UPDATE
-  LOOP
-    l_name    := l_item.name;
-    l_sell_in := l_item.sell_in;
-    l_quality := l_item.quality;
-
+  FOR l_item IN SELECT name, sell_in, quality, ctid FROM item FOR UPDATE LOOP
+    l_name := l_item.name; l_sell_in := l_item.sell_in; l_quality := l_item.quality;
     l_is_conjured := LEFT(l_name, 9) = 'Conjured ';
     l_base_name := CASE WHEN l_is_conjured THEN SUBSTRING(l_name FROM 10) ELSE l_name END;
-
     CASE
-      WHEN l_base_name = 'Sulfuras, Hand of Ragnaros' THEN
-        SELECT * FROM update_sulfuras(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
-      WHEN l_base_name = 'Aged Brie' THEN
-        SELECT * FROM update_aged_brie(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
-      WHEN l_base_name = 'Backstage passes to a TAFKAL80ETC concert' THEN
-        SELECT * FROM update_backstage_passes(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
-      ELSE
-        SELECT * FROM update_normal(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
+      WHEN l_base_name = 'Sulfuras, Hand of Ragnaros' THEN SELECT * FROM update_sulfuras(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
+      WHEN l_base_name = 'Aged Brie' THEN SELECT * FROM update_aged_brie(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
+      WHEN l_base_name = 'Backstage passes to a TAFKAL80ETC concert' THEN SELECT * FROM update_backstage_passes(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
+      ELSE SELECT * FROM update_normal(l_quality, l_sell_in, l_is_conjured) INTO l_new_quality, l_new_sell_in;
     END CASE;
-
-    l_quality := l_new_quality;
-    l_sell_in := l_new_sell_in;
-
-    UPDATE item
-      SET name = l_name, sell_in = l_sell_in, quality = l_quality
-      WHERE ctid = l_item.ctid;
+    UPDATE item SET name = l_name, sell_in = l_new_sell_in, quality = l_new_quality WHERE ctid = l_item.ctid;
   END LOOP;
 END;
 $$;
